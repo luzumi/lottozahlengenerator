768f0a4b4661eb6c3a763dce2f300d88a559b209
luzumi <corataca@gmail.com>
2023-06-30 18:56:23 +0200

    routes prefix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    <a href="/lotto/generate">
+    <a href="{{ route('generate') }}">
         <div class="content">
             <div class="title m-b-md">
                 Dein 6´er im Lotto
