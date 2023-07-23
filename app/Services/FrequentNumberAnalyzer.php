bd0e926f9b07965b0c5b145822778dc396f85ce5
luzumi <corataca@gmail.com>
2023-07-23 20:19:16 +0200

    composer

--- app/Services/FrequentNumberAnalyzer.php
+++ app/Services/FrequentNumberAnalyzer.php
@@ -8,8 +8,8 @@ class FrequentNumberAnalyzer
     {
         $numberWithCount = new NumberCount();
         $frequentNumbers = $numberWithCount->getNumbersWithCount(range(1, 49));
-
         $selectedNumbers = array_keys(collect($frequentNumbers)->sortDesc()->toArray());
+
         return array_slice($selectedNumbers, 0, 6);
     }
 }
