1393d3b40e01452e3ab4015d835042ad5b832a2f
luzumi <corataca@gmail.com>
2023-08-23 22:11:58 +0200

    dockerize

--- start.sh
+++ start.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
+# Warten Sie, bis die Datenbank verfügbar ist
+while ! mysqladmin ping -h"mariadb" -P"3306" --silent; do
+    sleep 1
+done
+
 # Navigieren Sie zum Laravel-Verzeichnis
 # shellcheck disable=SC2164
 cd /var/www/html
 
-# Aktualisieren Sie den Code von GitHub
-git pull origin master
-
-# Führen Sie die Migrationen durch (falls erforderlich)
-php artisan migrate
+# Führen Sie die Migrationen und das Seeding durch
+php artisan migrate --force
+php artisan db:seed
 
 # Starten Sie den Apache-Webserver
 exec apache2-foreground
