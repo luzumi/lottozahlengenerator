9fcdf787974069ed899fbd27574ac9870c6a8e5d
luzumi <corataca@gmail.com>
2023-08-23 19:45:15 +0200

    dockerize

---
+++ initialize.sh
@@ -0,0 +1,40 @@
+FROM php:8.1-apache
+
+# Aktivieren Sie die erforderlichen PHP-Erweiterungen
+RUN apt-get update -y && \
+    apt-get install -y libfreetype6-dev libjpeg62-turbo-dev libpng-dev libzip-dev && \
+    docker-php-ext-configure gd --with-freetype --with-jpeg && \
+    docker-php-ext-install gd && \
+    docker-php-ext-install pdo pdo_mysql && \
+    docker-php-ext-install zip
+
+# Installieren Sie den MySQL-Client
+RUN apt-get update && apt-get install -y default-mysql-client
+
+# Aktivieren Sie das Apache-Mod_rewrite
+RUN a2enmod rewrite
+
+# Kopieren Sie Ihre Laravel-App in den Container
+COPY .env.example /var/www/html/lotto/.env
+COPY ./ /var/www/html/lotto
+
+# Installieren Sie Composer
+RUN curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
+
+# Installieren Sie die Abhängigkeiten
+RUN composer install
+
+# Setzen Sie die Berechtigungen
+RUN chown -R www-data:www-data /var/www/html
+
+# Berechtigungen für das Speicherverzeichnis
+RUN chmod -R 775 /var/www/html/lotto/storage
+
+# Skript in das Image kopieren
+COPY initialize.sh /initialize.sh
+
+# Skript ausführbar machen
+RUN chmod +x /initialize.sh
+
+# Setzen Sie den öffentlichen Ordner als Root
+WORKDIR /var/www/html/lotto/public
